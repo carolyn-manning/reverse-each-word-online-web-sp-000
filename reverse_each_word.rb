142d6bc6445a25f75794207c85d3b53047451745
Carolyn Manning <carolyn.manning3@gmail.com>
2020-09-24 02:54:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   string_as_array = string.split
   string_as_array.collect do |word|
-    "#{word.reverse}"
+    word.reverse
   end
   string_as_array.join(" ")
 end
